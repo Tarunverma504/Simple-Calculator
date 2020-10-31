818f731d55cb174092c4e00b367040f049f12d18
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2020-10-31 10:04:06 +0530

    Update cal_js.js

--- cal_js.js
+++ cal_js.js
@@ -1,23 +1,57 @@
 let b='';
-function num(a)
-{
-	if(a=='=')
-	{
-		document.getElementById('history-value').innerHTML=b;
-		console.log(b);
+let x=0;
+function num(a){
+	if(a=='='){
+		console.log("b="+b);
+		var ans=calculate(b);
+		b=ans;
 	}
-	else if(a=='clr')
-	{
+	else if(a=='clr'){
 		document.getElementById('output-value').innerHTML='0';
 		b='';
 		a='';
-		console.clear();
 	}
-	else
-	{
+	else if(a=="bsp"){
+		if(b.length==1){
+			document.getElementById('output-value').innerHTML='0';
+		}
+		else{
+			b=b.toString();
+			console.log(typeof(b));
+			b=b.slice(0,b.length-1);
+			document.getElementById('output-value').innerHTML=b;
+		}
+	}
+	else{
 	b=b+a;
 	document.getElementById('output-value').innerHTML=b;
-	console.log(a);	
     }
-
+}
+function calculate(b){
+	try{
+		var i=0;
+		while(1){
+			if(b.charAt(i)>'0'||b.charAt(i)<'0')
+			{
+				break;
+			}
+			i++;
+		}
+		if(i>=b.length){
+			b=0;
+		}
+		else{
+			b=b.slice(i);
+		}
+		x=eval(b);
+		console.log(x);
+		document.getElementById('history-value').innerHTML=b;
+		document.getElementById('output-value').innerHTML=x;
+		return x;
+       }
+       catch(err){
+       	document.getElementById('history-value').innerHTML="ERROR";
+       	document.getElementById('output-value').innerHTML='0';
+       	return '0';
+       }
 }
